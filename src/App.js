59d4a17592a8c0e70b379244cd7fd302c6de0091
CarlW <carl@oskarroger.net>
2019-03-01 18:52:02 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World!</p>
+        <h1>Hello World !</h1>
       </div>
     );
   }
